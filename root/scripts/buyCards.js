4da5c850b76be7f5537a016a2e73c21421908f18
panevadov <panevadov@unal.edu.co>
2023-07-11 22:24:31 -0500

    New changes, show more and counter update

--- root/scripts/buyCards.js
+++ root/scripts/buyCards.js
@@ -1,20 +1,17 @@
 const containerCharacters = document.querySelector(".ContainerCards");
 const tarjetaCompleta = document.querySelector(".tarjetaCompleta");
-//const moreCards = document.querySelector(".moreCardsButton");
-//const moreCardsButton =document.querySelector('.Siguiente')
 
-
-
-
+//Inicialización de variables
 let url = 'https://pokeapi.co/api/v2/pokemon';
 let template = "";
 let initial = 0;
-let loadCards = 16;
+let loadCards = 20;
 
 let pokemones = [];
 
 //Crea la carta
 const showFirstLoad = (pokemones) => {
+    
     pokemones.forEach(async (pokemon) => {
         let Carta = document.createElement("div")
         Carta.className = "TarjetaIndividual";
@@ -23,52 +20,66 @@ const showFirstLoad = (pokemones) => {
                     <p>${pokemon.name}</p>
                     <i class="fa-sharp fa-regular fa-heart"></i>
                 </div>
-                <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" class="ImagenPokemon"></img>
+                <img src="${pokemon.sprites.other.home.front_default}" alt="${pokemon.name}" class="ImagenPokemon"></img>
                 <div>
                     <p><b>Exp ${pokemon.base_experience} </b></p>
                     <button class="BotonCompra">Buy</button>
                 </div>
             `;
             containerCharacters.appendChild(Carta); //El div de Carta se pone al contenedor de caracteres
+
+        //Para conteo de cartas
+        let misPokemones = JSON.parse(localStorage.getItem("pokemones"));
+        const numberCards =document.querySelector('.number');  //capturo la parte del conteo
+        numberCards.textContent= `${misPokemones.length} Cards`;  //muestro las cartas que se están mostrando en el momento
+
     });
 
     
 }
 
 //Carga la información para la tarjeta
-const loadCharacters = async () => {
+const loadCharacters = async (actualPosition = initial, limit = loadCards) => {
 
     try {
         pokemones = localStorage.getItem("pokemones");
 
-        if (!pokemones) {
-            const res = await fetch(url);
+        //if (!pokemones) {
+            //const res = await fetch(url);
+            const res = await fetch(url + '?offset='+actualPosition+'&limit='+limit);
             const data = await res.json();
 
             const pokemonData = await Promise.all(
                 //Recorrido para extraer la información
-                data.results.map(async (link) => {
-                    const urlPokemon = await fetch(link.url);
-                    console.log(urlPokemon);
+                data.results.map(async (pokemon) => {
+                    const urlPokemon = await fetch(pokemon.url);
                     return urlPokemon.json();
                 })
             );
 
             //Se almacena la información (nombre, tipo, experiencia y sprites) de pokemonData en pokemones
-            pokemones = pokemonData.map(
-                ({ name, sprites, types, base_experience, }) => ({
-                    name,
-                    sprites: sprites.other.home,
-                    types,
-                    base_experience,
+            pokemones = pokemonData.map((pokemon) => ({
+                    name: pokemon.name,
+                    sprites: pokemon.sprites,
+                    types: [],
+                    base_experience: pokemon.base_experience,
+                    id: pokemon.id,
                 })
             );
+            
+            let misPokemones = localStorage.getItem("pokemones");
+            if (!misPokemones) {
+                misPokemones = [];
+            } else {
+                misPokemones = JSON.parse(misPokemones);
+            }
+            const newPokemones = misPokemones.concat(pokemones);
 
             //Se convierte en información local
-            localStorage.setItem("pokemones", JSON.stringify(pokemones));
-        } else {
-            pokemones = JSON.parse(pokemones);
-        }
+            localStorage.setItem("pokemones", JSON.stringify(newPokemones));
+        // } else {
+        //     pokemones = JSON.parse(pokemones);
+        // }
 
         showFirstLoad(pokemones.slice(initial, loadCards));
 
@@ -79,12 +90,13 @@ const loadCharacters = async () => {
 
 loadCharacters();
 
-//Para conteo de cartas
-const numberCards =document.querySelector('.number');  //capturo la parte del conteo
-numberCards.textContent= `${pokemones.length} Cards`;  //muestro las cartas que se están mostrando en el momento
 
 //Para mostrar más cartas
-
+const siguiente = document.querySelector(".Siguiente");
+siguiente.addEventListener("click", function(){
+    const misPokemones = JSON.parse(localStorage.getItem("pokemones"));
+    loadCharacters(misPokemones.length, loadCards);
+});
 
 
 
